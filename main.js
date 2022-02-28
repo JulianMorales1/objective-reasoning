085b49d4d06ebcb499c078aa2f01edea4c553765
JulianMorales1 <98353349+JulianMorales1@users.noreply.github.com>
2022-02-28 17:42:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(user) {
+    if(user.userRole === "ADMIN") {
+        return true;
+    }else {
+        return false;
+    }
+}
 
+function getEmail(user){
+    return user.firstName
+    .concat(".", user.lastName, "@codeimmersives.com")
+    .toLowerCase();
+}
 
+function getPlaylistLength(list){
+    return list.songs.length;
+}
+
+function getHardestHomework(hardest){
+    let lowestScore = 9999999;
+    let nameOfHardestHw = "";
+
+    for(const item of hardest){
+
+        if(item.averageScore < lowestScore){
+
+            lowestScore = item.averageScore;
+            nameOfHardestHw = item.name;
+        }
+    }
+    return nameOfHardestHw;
+}
+
+function createPhonebook(firstNameArr, numArr, phonebook = {}){
+    for(let i = 0; i < firstNameArr.length; i ++){
+        let firstName = firstNameArr[i];
+        let phone = numArr[i];
+        phonebook[firstName] = phone;
+    }
+    return phonebook;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
